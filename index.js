9d62d2079488d3f12cbcda853787689c6fce1ad5
Max Dev <maxandersonmdr@gmail.com>
2025-02-11 02:43:33 +0100

    ss

--- index.js
+++ index.js
@@ -3,82 +3,104 @@
 const { types, scopes } = require('./values');
 
 const inquirer = require('inquirer').default;
-const args = process.argv.slice(2);
 const { execSync } = require('child_process');
 
 
-async function commit() {
-    console.clear();
-    console.log("Welcome to Committer. It's recommended to use only one type and only one scope per commit. 🚀😉");
+async function commit({ message, shouldPush, files }) {
 
-    const result = await inquirer.prompt([
-        {
+    console.clear();
+    console.log("Welcome to Committer.");
+
+    let commit_message;
+    const questions = []
+
+    if (!files) {
+        questions.push({
             name: "files",
             message: "Commit files",
             type: "input",
             default: "."
-        },
-        {
-            name: "types",
-            message: "Commit types",
-            type: "checkbox",
-            choices: types.map(c => c.title),
-            default: ["Update"],
-            loop: false,
-        },
-        {
-            name: "scopes",
-            message: "Commit scopes",
-            type: "checkbox",
-            choices: scopes.map(c => c.title),
-            loop: false,
+        },)
+    }
+    if (!message) {
+        console.log("It's recommended to use only one type and only one scope per commit. 🚀😉");
+        questions.push(
+            {
+                name: "types",
+                message: "Commit types",
+                type: "checkbox",
+                choices: types.map(c => c.title),
+                default: ["Update"],
+                loop: false,
+            },
+            {
+                name: "scopes",
+                message: "Commit scopes",
+                type: "checkbox",
+                choices: scopes.map(c => c.title),
+                loop: false,
+            },
+        )
+    }
+
+    const result = await inquirer.prompt(questions);
+
+    if (files) {
+        result.files = files;
+    }
+
+    if (message) {
+        commit_message = message;
+    } else {
+
+
+
+        const commit_types = types.filter(t => result.types.includes(t.title)).map(t => t.value);
+        const commit_scopes = scopes.filter(t => result.scopes.includes(t.title)).map(s => s.value);
+
+        if (commit_scopes.length == 0) {
+            const custom_scope = await inquirer.prompt([{
+                name: "scope",
+                message: "No scope choosen. Write you own scope ?",
+                type: "input",
+            }])
+            if (custom_scope.scope.trim() != "")
+                commit_scopes.push(custom_scope.scope);
         }
-    ])
 
-
-    const commit_types = types.filter(t => result.types.includes(t.title)).map(t => t.value);
-    const commit_scopes = scopes.filter(t => result.scopes.includes(t.title)).map(s => s.value);
-
-    if (commit_scopes.length == 0) {
-        const custom_scope = await inquirer.prompt([{
-            name: "scope",
-            message: "No scope choosen. Write you own scope ?",
+        const description = await inquirer.prompt([{
+            name: "description",
+            message: "Commit description",
             type: "input",
         }])
-        if (custom_scope.scope.trim() != "")
-            commit_scopes.push(custom_scope.scope);
+
+
+        function capitalize(str = "") {
+            if (str.trim() == "") return "";
+            return str[0].toUpperCase() + str.slice(1);
+        }
+        const out = {
+            type: commit_types.join(" & "),
+            scope: commit_scopes.length > 0 ? `(${commit_scopes.join("+")})` : "",
+            desc: description.description ? `: ${capitalize(description.description)}` : "",
+        }
+
+        commit_message = `${out.type}${out.scope}${out.desc}`;
+
     }
-
-    const description = await inquirer.prompt([{
-        name: "description",
-        message: "Commit description",
-        type: "input",
-    }])
-
-
-    function capitalize(str = "") {
-        if (str.trim() == "") return "";
-        return str[0].toUpperCase() + str.slice(1);
-    }
-    const out = {
-        type: commit_types.join(" & "),
-        scope: commit_scopes.length > 0 ? `(${commit_scopes.join("+")})` : "",
-        desc: description.description ? `: ${capitalize(description.description)}` : "",
-    }
-
-    const message = `${out.type}${out.scope}${out.desc}`;
 
     const command = {
         add: `git add ${result.files}`,
-        commit: `git commit -m "${message}"`
+        commit: `git commit -m "${commit_message}"`
     }
 
     for (let key in command) {
-        await execCommand(command[key])
+        execCommand(command[key])
     }
 
 
-    const action = await inquirer.prompt([{
+
+    let action = shouldPush ? { push: true } : await inquirer.prompt([{
         name: "push",
         message: "Push to default branch ?",
         type: "confirm",
@@ -100,9 +122,66 @@ function execCommand(command) {
 }
 
 
+const help = `
+Committer Help (committer -h):
+
+Quick Syntax :
+    committer "commit message" [-p] [-f <files>]
+    
+Prompt-based Syntax :
+    committer
+
+Args :
+    -p or --push, Wheither if should push after commit
+    -f or --files, Specify files to commit (spaced separated)
+`;
+
 async function main() {
     try {
-        await commit();
+        const args = process.argv.slice(2);
+        const options = {};
+        const keywords = ['-p', "--push", '-f', "--files"]
+        if (args.length > 0) {
+            if (args[0] == "-h") {
+                console.log(help);
+                return;
+            }
+            if (keywords.includes(args[0]) || args[0].trim() == "") {
+                console.error("Commit message required. Check help (-h)");
+                return;
+            }
+
+            options.message = args[0].trim();
+            options.shouldPush = args.includes("-p") || args.includes("--push");
+
+
+            function retrieveFiles(key) {
+                if (args.includes(key)) {
+                    const index = args.indexOf(key);
+                    return args.slice(index + 1);
+                }
+                return null;
+            }
+
+            let files = '.';
+
+            for (let key of ["-f", "--files"]) {
+                let retrieved = retrieveFiles(key);
+                if (retrieved) {
+                    files = retrieved.filter(f => !keywords.includes(f)).join(" ");
+                    break;
+                }
+            }
+
+            if (files.trim() == "") {
+                console.error("Commit files required. Check help (-h)");
+                return;
+            }
+
+            options.files = files;
+
+        }
+        await commit(options);
     } catch (err) {
         console.log(`Cannot commit because 👉 ${err.message}`);
         process.exit(1);
